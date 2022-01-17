ef1e06446594a37a7e9bcb8b692b657cf3ba8ea1
Valentin Hervieu <valentin@hervi.eu>
2022-01-17 21:11:11 +0100

    feat: add geolocation using Vercel middleware

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,16 @@
 import * as React from 'react'
-import type { NextPage } from 'next'
+import type { GetServerSideProps, InferGetServerSidePropsType, NextPage } from 'next'
 import Container from '@mui/material/Container'
 import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
 
-const Home: NextPage = () => {
+export const getServerSideProps: GetServerSideProps = async ({ query }) => ({
+  props: query,
+})
+
+type Props = InferGetServerSidePropsType<typeof getServerSideProps>
+
+const Home: NextPage<Props> = ({ city, region, country }) => {
   return (
     <Container maxWidth="lg">
       <Box
@@ -18,6 +24,17 @@ const Home: NextPage = () => {
       >
         <Typography variant="h4" component="h1" gutterBottom>
           Weather app
+        </Typography>
+        <Typography>
+          <b>City:</b> {city}
+        </Typography>
+
+        <Typography>
+          <b>Region:</b> {region}
+        </Typography>
+        <Typography>
+          {' '}
+          <b>Country:</b> {country}
         </Typography>
       </Box>
     </Container>
